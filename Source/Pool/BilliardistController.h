1960972ee31d1c987c4c22e017896341a195b383
Sharden <vickarchuck@yandex.ru>
2019-02-02 15:40:58 +0300

    Added custom player controller to move network code there

---
+++ Source/Pool/BilliardistController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BilliardistController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POOL_API ABilliardistController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
