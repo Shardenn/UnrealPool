8a1f68e86466f4a5c296ebc3f9b97849871f554f
Sharden <vickarchuck@yandex.ru>
2019-06-27 02:12:59 +0300

    Added new table mesh. Updated BP_Table. Removed bad dir GameObjects

--- Source/Pool/Table.h
+++ Source/Pool/Table.h
@@ -21,17 +21,17 @@ protected:
     // Called when the game starts or when spawned
     virtual void BeginPlay() override;
 
-    UPROPERTY(EditAnywhere, meta = (DisplayName = "Table mesh"))
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (DisplayName = "Table mesh"))
     UStaticMeshComponent* m_pTableMesh = nullptr;
 
-    UPROPERTY(EditAnywhere, Meta = (DisplayName = "Triangle front ball location"))
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Meta = (DisplayName = "Triangle front ball location"))
     USceneComponent* m_pFrontBallLocation = nullptr;
 
-    UPROPERTY(EditAnywhere, Meta = (DisplayName = "Ball fall registrator"))
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Meta = (DisplayName = "Ball fall registrator"))
     UBoxComponent* m_pBallRegistrator = nullptr;
 
     // Spline path for player movement along the table
-    UPROPERTY(EditAnywhere, Meta = (DisplayName = "Spline player path"))
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Meta = (DisplayName = "Spline player path"))
     USplineComponent* m_pSplinePath = nullptr;
 public:
     // Called every frame
