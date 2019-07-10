6f04f7e04f0f7fa1758e7a5a9e0a8dbda10714ad
Sharden <vickarchuck@yandex.ru>
2019-07-10 18:32:45 +0300

    Overriden PlayerRestart and some more funcs in GameMode. Moved old gamemode functionality from BP to CPP

--- Source/Pool/AmericanPool/PoolGameMode.cpp
+++ Source/Pool/AmericanPool/PoolGameMode.cpp
@@ -1,5 +1,77 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "PoolGameMode.h"
 
+#include "Table.h"
+#include "Billiardist.h"
+
+#include "EngineUtils.h"
+#include "Kismet/KismetMathLibrary.h"
+
+bool APoolGameMode::ReadyToStartMatch_Implementation()
+{
+    Super::ReadyToStartMatch();
+    return false;
+}
+
+void APoolGameMode::PostLogin(APlayerController* NewPlayer)
+{
+    PlayerControllers.Add(NewPlayer);
+    RestartPlayer(NewPlayer);
+}
+
+void APoolGameMode::RestartPlayer(AController* Controller)
+{
+    if (GameTable == nullptr)
+    {
+        bool bSuccess = InitializeTable();
+        if (!bSuccess)
+        {
+            UE_LOG(LogGameMode, Warning, TEXT("Did not find a game table"));
+            return;
+        }
+    }
+
+    FActorSpawnParameters SpawnInfo;
+    SpawnInfo.ObjectFlags |= RF_Transient;
+
+    UWorld* World = GetWorld();
+    auto Pawn = World->SpawnActor<APawn>(DefaultPawnClass, GetSpawnTransform());
+    
+    Cast<ABilliardist>(Pawn)->Initialize(GameTable->GetSplinePath());
+
+    Controller->Possess(Pawn);
+
+    Controller->ClientSetRotation(Controller->GetPawn()->GetActorRotation(), true);
+
+    UE_LOG(LogGameMode, Warning, TEXT("Number of players is %d"), NumPlayers);
+}
+
+bool APoolGameMode::InitializeTable()
+{
+    UWorld* World = GetWorld();
+    for (TActorIterator<ATable> It(World); It; ++It)
+    {
+        ATable* Table = *It;
+        if (Table)
+        {
+            this->GameTable = Table;
+            return true;
+        }
+    }
+    return false;
+}
+
+FTransform APoolGameMode::GetSpawnTransform()
+{
+    USplineComponent* TableSpline = GameTable->GetSplinePath();
+    
+    FVector Location = TableSpline->GetLocationAtDistanceAlongSpline(0,
+        ESplineCoordinateSpace::World);
+    FRotator Rotation = UKismetMathLibrary::FindLookAtRotation(Location, GameTable->GetActorLocation());
+    Rotation.Pitch = 0;
+    Rotation.Roll = 0;
+    
+    FTransform SpawnTransform;
+    SpawnTransform.SetLocation(Location);
+    SpawnTransform.SetRotation(FQuat(Rotation));
+    return SpawnTransform;
+}
--- Source/Pool/AmericanPool/PoolGameMode.h
+++ Source/Pool/AmericanPool/PoolGameMode.h
@@ -14,4 +14,23 @@ class POOL_API APoolGameMode : public AGameMode
 {
 	GENERATED_BODY()
 	
+public:
+    /**
+    * Overrides GameModeBase restartPlayer.
+    * Spawns player on table spline so that he looks at the table.
+    */
+    virtual void RestartPlayer(AController* Controller) override;
+    virtual bool ReadyToStartMatch_Implementation() override;
+    virtual void PostLogin(APlayerController* NewPlayer) override;
+
+    bool InitializeTable();
+
+protected:
+    class ATable* GameTable = nullptr;
+    TArray<AController*> PlayerControllers;
+
+private:
+    // Returns location at pline of the table and 
+    // "look at table" rotation
+    FTransform GetSpawnTransform();
 };
