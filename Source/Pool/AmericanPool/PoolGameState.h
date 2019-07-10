97800f968aa3d679688a39771bb6ed83f7b5aed0
Sharden <vickarchuck@yandex.ru>
2019-07-10 15:33:27 +0300

    Reparented gamemode and gamestate from BASE to full versions. Created those full versions

---
+++ Source/Pool/AmericanPool/PoolGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "PoolGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POOL_API APoolGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
